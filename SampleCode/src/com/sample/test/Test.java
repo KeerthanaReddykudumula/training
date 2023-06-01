b63abea539d3156e2c9a3dd184c00a3bdc6ff298
keert <keert@Keerthana>
2023-06-01 13:00:40 -0400

    Created new project SampleCode

---
+++ SampleCode/src/com/sample/test/Test.java
@@ -0,0 +1,10 @@
+package com.sample.test;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is the Sample Program");
+	}
+
+}
